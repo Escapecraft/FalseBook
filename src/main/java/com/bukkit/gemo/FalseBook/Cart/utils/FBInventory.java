a9a0d498276a73dfb137046ed52634928f653d62
James <beardyjames@live.co.uk>
2013-09-29 00:25:19 +0100

    1.6.4 imports fix

--- src/main/java/com/bukkit/gemo/FalseBook/Cart/utils/FBInventory.java
+++ src/main/java/com/bukkit/gemo/FalseBook/Cart/utils/FBInventory.java
@@ -1,9 +1,9 @@
 package com.bukkit.gemo.FalseBook.Cart.utils;
 
-import net.minecraft.server.v1_6_R2.Container;
-import net.minecraft.server.v1_6_R2.EntityHuman;
-import net.minecraft.server.v1_6_R2.InventoryCrafting;
-import net.minecraft.server.v1_6_R2.ItemStack;
+import net.minecraft.server.v1_6_R3.Container;
+import net.minecraft.server.v1_6_R3.EntityHuman;
+import net.minecraft.server.v1_6_R3.InventoryCrafting;
+import net.minecraft.server.v1_6_R3.ItemStack;
 
 public class FBInventory extends InventoryCrafting {
 
